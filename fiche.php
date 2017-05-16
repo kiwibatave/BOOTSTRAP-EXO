bf6964b232887435a891db4fe98e2da4e373a3f3
kiwibatave <b_cuzon@yahoo.fr>
2017-05-16 08:43:06 +0200

    first push

---
+++ fiche.php
@@ -0,0 +1,13 @@
+<!DOCTYPEhtml>
+<htlm>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <p><?php
+      echo $_POST["Name"];
+      echo"<br>" , $_POST["Email"];
+      echo"<br>" , $_POST["Message"];
+    ?></p>
+  </body>
+</html>
